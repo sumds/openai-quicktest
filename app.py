824f4180143b97c29fc86350b71ee8892f9c5cb7
sumds <catsumd@gmail.com>
2023-09-29 17:48:50 +0530

    app.py modified prompt_message recontructed simple js file is passed to generate test cases and output.txt added

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import os
 import configparser
 
 api_key = os.environ.get("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("No OPENAI_API_KEY found in environment variables!")
+
 
 file_path = r'E:\generative-ai\simple-login-app\src\utils\validation.js'
 
-print(api_key)
+openai.api_key = api_key
 
 # Step 1: Read the content from the file
 with open(file_path, 'r') as file:
@@ -14,30 +17,26 @@ with open(file_path, 'r') as file:
 
 #Step 2:Construct the prompt
 #By specifying "```javascript", you're helping the model to understand that the content is JavaScript code. The clearer the context you provide, the better the potential outcomes from the model.
-# prompt_message = (
-#     "Given the following JavaScript application code, please generate Selenium test cases in Python that validate its functionality:\n\n"
-#     "```javascript\n" + file_content + "\n```\n"
-#     "Expected format:\n"
-#     "- Use Python for the Selenium scripts.\n"
-#     "- Utilize commands like 'driver.findElement', 'driver.click', etc.\n"
-#     "Begin Test Cases:\n"
-# )
-
 prompt_message = (
     "Given the following test scenarios for a web login page, generate corresponding Selenium test cases written in Python:\n\n"
     + file_content + "\n"
     "Please use Chrome as the web driver and provide complete scripts."
 )
-print(prompt_message)
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant that generates Selenium test cases based on given JavaScript code."},
+    {"role": "user", "content": prompt_message}
+]
 
 # Use the completion endpoint to get a response from the model
-response = openai.Completion.create(
-    model="davinci",  # or other engines like "curie"
-    prompt=prompt_message,
-    max_tokens=1700
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages
 )
 
-# Print the response
+# Print and save the response
+
+
 output_file_path = 'output.txt'
 with open(output_file_path, 'w', encoding="utf-8") as output_file:
-    output_file.write(response.choices[0].text)
+    output_file.write(response.choices[0].message['content'])
